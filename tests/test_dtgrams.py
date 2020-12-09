759ac16d2aeb4abe3b313ca4526276e1f9466bcc
Benjamin Murauer <b.murauer@gmail.com>
2020-12-09 13:50:40 +0100

    init

--- tests/test_dtgrams.py
+++
@@ -1,5 +0,0 @@
-from dtgrams import __version__
-
-
-def test_version():
-    assert __version__ == '0.1.0'
--- tests/test_treegrams.py
+++ tests/test_treegrams.py
@@ -1,41 +1,38 @@
 import unittest
 
-from dtgrams.transformers import TreeGramExtractor
+from treegrams.transformers import TreeGramExtractor
+from nltk import Tree
 
+def test_treegram():
+    transformer = TreeGramExtractor("PQGram", p=2, q=3)
 
-class TestTreegrams(unittest.TestCase):
-    def test_treegram(self):
-        transformer = TreeGramExtractor("PQGram", p=2, q=3)
-
-        documents = [
-            [
-                [
-                    "(ROOT (INNER (INNER (DET Der) (NOUN Hund)) (VERB jagte) "
-                    "(INNER (DET die) (NOUN Katze)) (. .)))"
-                ]
-            ]
+    documents = [
+        [
+            Tree.fromstring("(ROOT (INNER (INNER (DET Der) (NOUN Hund)) "
+                "(VERB jagte) (INNER (DET die) (NOUN Katze)) (. .)))")
         ]
+    ]
 
-        expected = [
-            [
-                "*_ROOT_*_*_INNER",
-                "ROOT_INNER_*_*_INNER",
-                "INNER_INNER_*_*_DET",
-                "INNER_INNER_*_DET_NOUN",
-                "INNER_INNER_DET_NOUN_*",
-                "INNER_INNER_NOUN_*_*",
-                "ROOT_INNER_*_INNER_VERB",
-                "ROOT_INNER_INNER_VERB_INNER",
-                "INNER_INNER_*_*_DET",
-                "INNER_INNER_*_DET_NOUN",
-                "INNER_INNER_DET_NOUN_*",
-                "INNER_INNER_NOUN_*_*",
-                "ROOT_INNER_VERB_INNER_.",
-                "ROOT_INNER_INNER_._*",
-                "ROOT_INNER_._*_*",
-                "*_ROOT_*_INNER_*",
-                "*_ROOT_INNER_*_*",
-            ]
+    expected = [
+        [
+            "*_ROOT_*_*_INNER",
+            "ROOT_INNER_*_*_INNER",
+            "INNER_INNER_*_*_DET",
+            "INNER_INNER_*_DET_NOUN",
+            "INNER_INNER_DET_NOUN_*",
+            "INNER_INNER_NOUN_*_*",
+            "ROOT_INNER_*_INNER_VERB",
+            "ROOT_INNER_INNER_VERB_INNER",
+            "INNER_INNER_*_*_DET",
+            "INNER_INNER_*_DET_NOUN",
+            "INNER_INNER_DET_NOUN_*",
+            "INNER_INNER_NOUN_*_*",
+            "ROOT_INNER_VERB_INNER_.",
+            "ROOT_INNER_INNER_._*",
+            "ROOT_INNER_._*_*",
+            "*_ROOT_*_INNER_*",
+            "*_ROOT_INNER_*_*",
         ]
-        actual = transformer.transform(documents)
-        self.assertEqual(actual, expected)
+    ]
+    actual = transformer.transform(documents)
+    assert actual == expected
